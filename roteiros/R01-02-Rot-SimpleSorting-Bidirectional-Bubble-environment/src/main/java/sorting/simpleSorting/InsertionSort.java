3a1133db7edefae016fc5090666706bd50cfbf95
paulorpn <paulo.ricardo.pereira.nascimento@ccc.ufcg.edu.br>
2023-08-15 15:39:29 -0300

    All set.

--- roteiros/R01-02-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/simpleSorting/InsertionSort.java
+++ roteiros/R01-02-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/simpleSorting/InsertionSort.java
@@ -13,7 +13,7 @@ public class InsertionSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		for (int i = leftIndex; i < rightIndex; i++) {
+		for (int i = leftIndex; i <= rightIndex; i++) {
 			
 			int j = i;
 
