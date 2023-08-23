aa9c90149134d7ced712b605d48e702b84169fa9
122110535 PAULO RICARDO PEREIRA DO NASCIMENTO <paulo.ricardo.pereira.nascimento@ccc.ufcg.edu.br>
2023-08-23 09:58:34 -0300

    roteiro 3

---
+++ roteiros/R03-02-Rot-Order-statistics-selection-environment/src/main/java/orderStatistic/OrderStatisticsSelectionImpl.java
@@ -0,0 +1,51 @@
+package orderStatistic;
+
+public class OrderStatisticsSelectionImpl<T extends Comparable<T>> implements OrderStatistics<T> {
+
+	/**
+	 * Esta eh uma implementacao do calculo da estatistica de ordem seguindo a estrategia 
+	 * de usar o selection sem modificar o array original. Note que seu algoritmo vai 
+	 * apenas aplicar sucessivas vezes o selection ate encontrar a estatistica de ordem 
+	 * desejada sem modificar o array original. 
+	 * 
+	 * Restricoes:
+	 * - Voce NÃO pode modificar o aray original
+	 * - Voce NÃO pode usar memória extra: nenhum array auxiliar deve ser criado e utilizado.
+	 * - Você NÃO pode usar métodos já prontos de manipulação de arrays
+	 * - Voce NÃO pode encontrar a k-esima estatistica de ordem por contagem de
+	 *   elementos maiores/menores, mas sim aplicando sucessivas selecoes (selecionar um elemento
+	 *   usando a ideia do selection sort).
+	 * - Considere que k varia de 1 a N 
+	 * - Voce DEVE usar recursão para resolver o problema. Nenhum laço poderá ser utilizado.
+	 * - Você pode implementar métodos auxiliares, desde que seja apenas nesta classe.
+	 * - Os métodos auxiliares NÃO PODEM ter mais que três parâmetros.
+	 *
+	 * Dica: procure pensar como usar a ideia do selection sort para resolver esse problema,
+     *       identifique que métodos você precisará para resolver o problema 
+	 *       e pense no template da recursão em cada método que voce vai implementar.
+	 */
+	@Override
+	public T getOrderStatistics(T[] array, int k) {
+
+		if (k >= 1) {
+			selection(array, 0, k);
+		}
+
+		getOrderStatistics(array, k--);
+
+		return array[selection(array, 0, k)];
+	}
+
+	private int selection(T[] array, int indiceMenor, int k) {
+
+		if (k >= 1) {
+			for (int i = 1; i < array.length; i++) {
+				if (array[i].compareTo(array[0]) < 0 && i != indiceMenor) {
+					indiceMenor = i;
+				}
+			}
+		}
+
+		return indiceMenor;
+	} 
+}
